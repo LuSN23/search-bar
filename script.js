bd4e44ceab77cc1d030845921d0c0069d50c0292
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-02-04 00:07:26 -0300

    Putting everything inside the loop of the forEach, adding attributes for the body and the header of the html's card, linking the attributes data-header and data-body to the script inside consts header and body, accessing specific properties name(header) and email(body) in the JSON file

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ const userCardTemplate = document.querySelector("[data-user-template]");
 fetch("https://jsonplaceholder.typicode.com/users") 
 .then(res => res.json()) //A response that will be converted to JSON
 .then(data => {
-     //Make a deep clone of all nodes inside(their contents) and stores inside the const card  
-    const card = userCardTemplate.content.cloneNode(true).children[0];//Getting the first child that is the card with all the content inside
-    /*console.log(card); //This will return something called #document-fragment, 
-    and in order to use the content we just need to get the first child*/
+  //Putting everything inside a loop of an array method forEach
+  data.forEach(user => {
+  //Make a deep clone of all nodes inside(their contents) and stores inside the const card  
+  const card = userCardTemplate.content.cloneNode(true).children[0];//Getting the first child that is the card with all the content inside
+  /*console.log(card); //This will return something called #document-fragment, 
+  and in order to use the content we just need to get the first child*/
+  //Getting the header(name) and body(email) to show in the HTML elements through the attributes
+  const header = card.querySelector("[data-header]");
+  const body = card.querySelector("[data-body]");
+  header.textContent = user.name; //Taking only the property that we want to show in the html's card
+  body.textContent = user.email; //When is setted the textContent replace any child nodes with the string specified
+  console.log(user); //We are going to be using just the name and email properties
+  })
 }) //Other conversion to get our data
 
